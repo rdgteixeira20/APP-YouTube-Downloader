442061b1960c235ec6fe9cd57819f9c72d654d5b
Rodrigo Teixeira <87489054+rdgteixeira20@users.noreply.github.com>
2023-04-02 20:21:15 -0300

    Update code
    Adicionado função download de audio em mp3 de vídeo do youtube

--- code.py
+++ code.py
@@ -2,10 +2,24 @@ from pytube import YouTube
 import os
 
 url = "https://www.youtube.com/watch?v=xCe9wnBjIcs"
+
+
 yt = YouTube(url)
+
+
 
 ### Download formato .mp4 (Vídeo) do YouTube ###
 video = yt.streams.get_highest_resolution()
 video.download()
 
+
+
+### Download formato .mp3 (música) do YouTube ###
+audio = yt.streams.filter(only_audio=True).first()
+out_file = audio.download()
+# Salvando arquivo formato .mp3
+base, ext = os.path.splitext(out_file)
+new_file = base + '.mp3'
+os.rename(out_file, new_file)
+# Retorno para confirmação
 print(yt.title + " has been successfully downloaded.")
